706fea256a57578925c3ca02e659d073df6cbeb8
Cody Clark <codylcl@yahoo.com>
2023-06-16 14:51:25 -1000

    added contact

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
       <section className="pressure-washing-quote">Give us a call or email for a quick quote</section>
       <section className="contact-buttons">
         <a className="contact-link" href="tel:example"><AiFillPhone className="phone-icon"/>&nbsp; (954) 389-2642</a>
-        <a className="contact-link" href="mailto: abc@example.com"><HiEnvelope className="email-icon"/>&nbsp; Get a Quote</a>
+        <a className="contact-link" href="mailto: pure_cleaning@live.com"><HiEnvelope className="email-icon"/>&nbsp; Get a Quote</a>
       </section>
     </div>
   );
